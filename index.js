3ccbb5943c5c6dbfbde616df0054951f9fd6e176
Mohammad-Labib <friendlabib@gmail.com>
2026-06-29 19:29:52 +0600

    add some server file

--- index.js
+++ index.js
@@ -26,10 +26,14 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
-    try {
-        // Connect the client to the server
-        await client.connect();
+// async function run() {
+//     try {
+//         // Connect the client to the server
+//         await client.connect();
+
+client.connect(() => {
+    console.log("connecting to mongoDB");
+}).catch(console.dir)
 
         const database = client.db("startup_forge");
         const founderCollection = database.collection("founders");
@@ -54,17 +58,19 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch (error) {
-        console.error("MongoDB Connection Error:", error);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
-}
-run().catch(console.dir);
+        // await client.db("admin").command({ ping: 1 });
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } catch (error) {
+//         console.error("MongoDB Connection Error:", error);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         // await client.close();
+//     }
+// }
+// run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
